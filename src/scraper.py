1488d15a1d5b51510d80fb33e27fe7d808092ad6
jojowither <ms0421555@gmail.com>
2021-04-26 15:30:33 +0800

    query the place in google map

--- src/scraper.py
+++ src/scraper.py
@@ -8,8 +8,8 @@ from pathlib import Path
 
 
 ind = {'most_relevant' : 0 , 'newest' : 1, 'highest_rating' : 2, 'lowest_rating' : 3 }
-HEADER = ['id_review', 'caption', 'relative_date', 'retrieval_date', 'rating', 'username', 'n_review_user', 'n_photo_user', 'url_user']
-HEADER_W_SOURCE = ['id_review', 'caption', 'relative_date','retrieval_date', 'rating', 'username', 'n_review_user', 'n_photo_user', 'url_user', 'url_source']
+HEADER = ['id_review', 'caption', 'relative_date', 'retrieval_date', 'rating', 'username', 'n_review_user', 'n_photo_user', 'url_user', 'place']
+HEADER_W_SOURCE = ['id_review', 'caption', 'relative_date','retrieval_date', 'rating', 'username', 'n_review_user', 'n_photo_user', 'url_user', 'place', 'url_source']
 
 def csv_writer(source_field, ind_sort_by, path='../data/'):
     outfile= ind_sort_by + '_gm_reviews.csv'
@@ -25,10 +25,12 @@ def csv_writer(source_field, ind_sort_by, path='../data/'):
     return writer
 
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Google Maps reviews scraper.')
     parser.add_argument('--N', type=int, default=100, help='Number of reviews to scrape')
     parser.add_argument('--i', type=str, default='../urls.txt', help='target URLs file')
+    parser.add_argument('--q', type=str, default='../query.txt', help='query file')
     parser.add_argument('--sort_by', type=str, default='newest', help='sort by most_relevant, newest, highest_rating or lowest_rating')
     parser.add_argument('--place', dest='place', action='store_true', help='Scrape place metadata')
     parser.add_argument('--debug', dest='debug', action='store_true', help='Run scraper using browser graphical interface')
@@ -41,30 +43,34 @@ if __name__ == '__main__':
     writer = csv_writer(args.source, args.sort_by)
 
     with GoogleMapsScraper(debug=args.debug) as scraper:
-        with open(args.i, 'r') as urls_file:
-            for url in urls_file:
-                if args.place:
-                    print(scraper.get_account(url))
-                else:
+        # query the place in Google Map
+        with open(args.q, 'r') as query_file:
+            for query in query_file:
+                error = scraper.search_place(query)
+                print(error)
+                if error == 0:
+                    urls_dict = scraper.get_all_place(query)
+
+                for place, url in urls_dict.items():
                     error = scraper.sort_by(url, ind[args.sort_by])
                     print(error)
 
-                if error == 0:
+                    if error == 0:
+                        n = 0
+                        if ind[args.sort_by] == 0:
+                            scraper.more_reviews()
+                            
+                        while n < args.N:
+                            print(colored('[Review ' + str(n) + ']', 'cyan'))
+                            reviews = scraper.get_reviews(n)
 
-                    n = 0
+                            for r in reviews:
+                                row_data = list(r.values())
+                                row_data.append(place)
+                                if args.source:
+                                    row_data.append(url[:-1])
 
-                    if ind[args.sort_by] == 0:
-                        scraper.more_reviews()
-                        
-                    while n < args.N:
-                        print(colored('[Review ' + str(n) + ']', 'cyan'))
-                        reviews = scraper.get_reviews(n)
+                                writer.writerow(row_data)
 
-                        for r in reviews:
-                            row_data = list(r.values())
-                            if args.source:
-                                row_data.append(url[:-1])
+                            n += len(reviews)
 
-                            writer.writerow(row_data)
-
-                        n += len(reviews)
